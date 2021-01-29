87947dbaf716197ab378ca18b8ec222f4885d8e3
Alper Donmez <alper.donmez490@gmail.com>
2021-01-29 09:44:48 -0500

    Fix firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,17 +3,16 @@ const firebase = require('firebase');
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
     projectId: process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
     appId: process.env.APP_ID,
     measurementId: process.env.MEASUREMENT_ID
-
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.database();
-const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-module.exports = { firebase, googleAuthProvider, db };
+module.exports = { db };
